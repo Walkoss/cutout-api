58c1faea92286cf7ca30a0966fe9bd52d05bf2b7
Walkoss <walkoss@users.noreply.github.com>
2017-09-15 20:53:34 +0200

    Updated JWT token TTL and Provider's description

--- src/AppBundle/DataFixtures/ORM/LoadProviderData.php
+++ src/AppBundle/DataFixtures/ORM/LoadProviderData.php
@@ -34,7 +34,11 @@ class LoadProviderData extends AbstractFixture implements ContainerAwareInterfac
         $provider->setIban($faker->iban('fr_FR'));
         $provider->setIsFreelance(false);
         $provider->setLocation($this->getReference('LOCATION_1'));
-        $provider->setDescription($faker->text);
+        $provider->setDescription('La coiffure est notre passion, faire de votre instant coiffure un moment de détente pour vous, une véritable pause, afin de vous rendre unique.
+
+Nous attachons le plus grand soin à la qualité de nos services, nous prenons le temps de vous écouter afin de vous proposer ce qu’il vous conviendra le mieux en respectant votre personnalité.
+
+Faites confiance à nos coiffeurs stylistes certifiés pour révéler la beauté de vos cheveux et mettre en valeur votre capital séduction.');
         $provider->setSiret($faker->siret);
         $provider->setRange(1);
         $provider->setProviderType($this->getReference(ProviderType::COIFFEUR));
